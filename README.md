b1bf7d97d263b83d99378c9c8216d1c7a0cad071
still-coding <get_lost@whatever.com>
2023-07-05 13:27:15 +0500

    fix code, add readme example

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# report_demo
-Демонстрационный пример отчёта в Markdown
+# Демонстрационный пример отчёта в Markdown
+
+## Задание
+
+Приближённо вычислить число $ \pi $ с помощью [произведения Валлиса](https://ru.wikipedia.org/wiki/%D0%A4%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D0%B0_%D0%92%D0%B0%D0%BB%D0%BB%D0%B8%D1%81%D0%B0):
+
+$$ \frac{\pi}{2} = \prod_{i=1}^{\infty} \frac{4i^2}{4i^2 - 1}. $$
+
+## Результаты вычислений
+
+Очередной множитель произведения на Python:
+
+```python
+def wallis_multiplier(i):
+    i_squared_times_4 = 4 * i * i
+    return i_squared_times_4 / (i_squared_times_4 - 1)
+```
+
+При разнице последних значений произведения $ \pi / 2 $ менее `1e-7` вычисленное значение $ \pi = 3.1411963131348553 $.
+
+![Pi convergence plot](img/convergence_plot.png)
+
+Произвденение Валлиса сходится очень медленно, поэтому рекомендуется использовать более эффективные методы вычисления числа $ \pi $.
+
+
+## Список использованных источников:
+
+1. [Matplotlib cheatsheets and handouts](https://matplotlib.org/cheatsheets/)
+2. [Markdown Cheat Sheet](https://www.markdownguide.org/cheat-sheet/)
--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-
-convergence_sequence = []
-
 def wallis_multiplier(i):
-    return (4 * i * i) / (4 * i * i - 1)
+    i_squared_times_4 = 4 * i * i
+    return i_squared_times_4 / (i_squared_times_4 - 1)
 
 
 def calculate_wallis_product(eps):
+    convergence_sequence = []
     i = 1
     p = 1
     while True:
@@ -19,28 +18,28 @@ def calculate_wallis_product(eps):
         p *= m
         convergence_sequence.append(p*2)
         i += 1
-    return p * 2
+    return p * 2, convergence_sequence
 
 
-def visualize_convergence():
+def visualize_convergence(sequence):
     fig, ax = plt.subplots()
     ax.scatter(
-        range(len(convergence_sequence)),
-        convergence_sequence,
-        s = 1
+        range(len(sequence)),
+        sequence,
+        s = 1,
     )
     ax.set(
         ylim=(3.1, 3.15),
         xlabel = 'Iterations',
         ylabel = 'Wallis Product value',
         title = 'Pi Convergence plot',
-
     )
+    plt.grid(alpha=0.7)
+    plt.savefig('./img/convergence_plot.png')
     plt.show()
 
 
 if __name__ == '__main__':
-    print(calculate_wallis_product(1e-7))
-    visualize_convergence()
-
-# https://matplotlib.org/cheatsheets/
+    pi_value, conv_seq = calculate_wallis_product(1e-7)
+    print('Pi calculated with Wallis product:', pi_value)
+    visualize_convergence(conv_seq)
